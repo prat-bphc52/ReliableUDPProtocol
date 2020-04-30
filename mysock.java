7d80c454291c51d7e56ebe80f8a04a5659ead917
Prateek Agarwal <hp.agarwal1@gmail.com>
2020-05-01 02:09:43 +0530

    Add packet verification SHA1 key

--- mysock.java
+++ mysock.java
@@ -8,16 +8,21 @@ import java.math.BigInteger;
 
 class MyReliableUDPSocket extends DatagramSocket{
 
+	private static int HASH_SIZE = 20; // SHA1 Hash
+	
+	private static int MAX_PACKET_DATA_LENGTH = 480;// in bytes
 
-	private static int MAX_PACKET_DATA_LENGTH = 500;// in bytes
-	final static int MAX_PACKET_SIZE = Integer.BYTES+Long.BYTES+MAX_PACKET_DATA_LENGTH;
-	// 1st 4 bytes seq no, next 8 bytes packet id, 512 bytes
-	private static int TRANSMISSION_ID_PACKET_LENGTH = Integer.BYTES + Long.BYTES + Integer.BYTES + Integer.BYTES; 
-	// 20 bytes
-	// Integer for sequence number, Long for transmission ID, Integer Total number of packets, Integer Total data length of byte array
+	final static int MAX_PACKET_SIZE_WITHOUT_HASH = Integer.BYTES+Long.BYTES+MAX_PACKET_DATA_LENGTH;
+	// 1st 4 bytes seq no, next 8 bytes packet id, 480 bytes data : Total = 492 Bytes
+	
+	final static int MAX_PACKET_SIZE = MAX_PACKET_SIZE_WITHOUT_HASH + HASH_SIZE;
 
-	private static int ACK_PCKT_DATA_LENGTH = Integer.BYTES + Long.BYTES;
-	// Seq no. in negative (to identify that these are acknowledgements)
+	private static int TRANSMISSION_ID_PACKET_LENGTH = Integer.BYTES + Long.BYTES + Integer.BYTES + Integer.BYTES + HASH_SIZE;
+	// 40 bytes
+	// Integer for sequence number, Long for transmission ID, Integer Total number of packets, Integer Total data length of byte array, 20 bytes for Hash
+
+	private static int ACK_PCKT_DATA_LENGTH = Integer.BYTES + Long.BYTES + HASH_SIZE;
+	// Seq no. in negative (to identify that these are acknowledgements) - 32 Bytes
 
 	public MyReliableUDPSocket(int port, InetAddress hostIP)throws SocketException{
 		super(port, hostIP);
@@ -37,7 +42,7 @@ class MyReliableUDPSocket extends DatagramSocket{
 		byte[] temp;
 		int count = 0;
 
-		// seq number for transmission ID packet = 1
+		// 4 bytes for seq number for transmission ID packet = 1
 		temp = inttoBytes(1);
 		for(int i=0;i<4;i++)
 			idArr[count++]=temp[i];
@@ -69,14 +74,16 @@ class MyReliableUDPSocket extends DatagramSocket{
 		for(int i=0;i<4;i++)
 			idArr[count++]=temp[i];
 
+		temp = getHash(idArr, 0 ,20);
+		for(int i=0;i<HASH_SIZE;i++)
+			idArr[count++]=temp[i];
 
 		DatagramPacket idPacket = new DatagramPacket(idArr, idArr.length, destAddr, destPort);
 		ths.map.put(1, idPacket);
 
 		ths.startReceiverThread(this);
 
-        System.out.println("Sending Seq 0 packet");
-        System.out.println("Packet ID : Timestamp - "+ ths.packetID);
+        System.out.println("Starting file sharing,  ID :"+ ths.packetID);
         try{
         	send(idPacket);
 		} catch(Exception e){
@@ -87,7 +94,7 @@ class MyReliableUDPSocket extends DatagramSocket{
         for(int i=0;i<ths.packetCount;i++){
         	DatagramPacket dp = createDataPacket(i+2, pc_id, arr, i*MAX_PACKET_DATA_LENGTH,destAddr, destPort);
         	ths.map.put(i+2, dp);
-        	System.out.println("Sending Seq "+(i+2)+" packet");
+        	// System.out.println("Sending Seq "+(i+2)+" packet");
         	try{
         		send(dp);
 			} catch(Exception e){
@@ -113,13 +120,13 @@ class MyReliableUDPSocket extends DatagramSocket{
 					} catch(Exception e){
         				System.out.println("Failed to send data");
         			}
-        			System.out.println("Sending again Seq "+i+" packet");
+        			// System.out.println("Sending again Seq "+i+" packet");
         			flag = false;
         		}
         	}
         }while(!flag);
         ths.stopReceiverThread();
-        System.out.println("End sending data");
+        // System.out.println("End sending data");
         return 0;
 	}
 
@@ -131,6 +138,8 @@ class MyReliableUDPSocket extends DatagramSocket{
 				buf = new byte[MAX_PACKET_SIZE];
 				DatagramPacket recv = new DatagramPacket(buf, buf.length);
         		receive(recv);
+        		if(isCorruptPacket(recv))
+        			continue;
         		int seqNo = getIntFromByteArray(buf, 0);
         		long pID = getLongFromByteArray(buf ,4);
         		if(thr.packetID==0){
@@ -139,7 +148,7 @@ class MyReliableUDPSocket extends DatagramSocket{
         		}
         		else if(thr.packetID!=pID || !thr.senderIP.equals(recv.getAddress().toString()))//to receive only the current transmission data
         			continue;
-        		System.out.println("\n**Received a packet** \nAddress:"+recv.getAddress()+" Port"+recv.getPort()+" Seq no "+seqNo +" Packet ID"+pID);
+        		// System.out.println("\n**Received a packet** \nAddress:"+recv.getAddress()+" Port"+recv.getPort()+" Seq no "+seqNo +" Packet ID"+pID);
         		send(createAckPacket(seqNo*-1,thr.packetID,recv.getAddress(),recv.getPort()));
         		if(seqNo>0){
         			if(!thr.map.containsKey(seqNo)){
@@ -172,9 +181,31 @@ class MyReliableUDPSocket extends DatagramSocket{
     		int varc = 12;
     		do{
     			buf[dc++] = dp.getData()[varc++];
-    		}while(varc != MAX_PACKET_SIZE && dc != thr.dataLength);
+    		}while(varc != MAX_PACKET_SIZE_WITHOUT_HASH && dc != thr.dataLength);
     	}
+        System.out.println("\n**File recevied and saved successfully**\nThank you for using MyReliableUDPSocket");
 		return buf;
+	}
+
+	public static boolean isCorruptPacket(DatagramPacket dpc){
+		int seqNo = getIntFromByteArray(dpc.getData(), 0);
+		int pckHashStart = 0;
+		if(seqNo<0){
+			pckHashStart = ACK_PCKT_DATA_LENGTH - HASH_SIZE;
+		}
+		else if(seqNo == 1){
+			pckHashStart = TRANSMISSION_ID_PACKET_LENGTH - HASH_SIZE;
+		}
+		else{
+			pckHashStart = MAX_PACKET_SIZE_WITHOUT_HASH;
+		}
+		byte[] hash;
+		hash = getHash(dpc.getData(),0, pckHashStart);
+		for(int i=0;i<HASH_SIZE;i++){
+			if(hash[i]!=dpc.getData()[i+pckHashStart])
+				return true;
+		}
+		return false;
 	}
 
 	public static byte[] longtoBytes(long data) {
@@ -214,13 +245,11 @@ class MyReliableUDPSocket extends DatagramSocket{
 	}
 
 	private DatagramPacket createDataPacket(int seqNo, byte[] id, byte[] data, int start, InetAddress destAddr, int destPort){
-		int packet_size = Integer.BYTES+Long.BYTES;
 		int end = start + MAX_PACKET_DATA_LENGTH;
 		if(end>data.length)
 			end = data.length;
-		packet_size+=(end-start);
 
-		byte[] pckt = new byte[packet_size];
+		byte[] pckt = new byte[MAX_PACKET_SIZE];
 		int count = 0;
 		// adding sequence number
 		byte[] temp = inttoBytes(seqNo);
@@ -234,6 +263,13 @@ class MyReliableUDPSocket extends DatagramSocket{
 		// adding data
 		for(int i=start;i<end;i++)
 			pckt[count++]=data[i];
+		while(count<MAX_PACKET_SIZE_WITHOUT_HASH)
+			pckt[count++]=0;
+
+		temp = getHash(pckt, 0, MAX_PACKET_SIZE  - HASH_SIZE);
+		// adding hash
+		for(int i=0;i<HASH_SIZE;i++)
+			pckt[count++]=temp[i];		
 
 		return new DatagramPacket(pckt, pckt.length, destAddr, destPort);
 	}
@@ -252,36 +288,26 @@ class MyReliableUDPSocket extends DatagramSocket{
 		for(int i=0;i<8;i++)
 			pckt[count++]=temp[i];
 
+		temp = getHash(pckt, 0, ACK_PCKT_DATA_LENGTH  - HASH_SIZE);
+		// adding hash
+		for(int i=0;i<HASH_SIZE;i++)
+			pckt[count++]=temp[i];	
+
 		return new DatagramPacket(pckt, pckt.length, destAddr, destPort);
 	}
 
-	public static String getMd5(String input) 
+	public static byte[] getHash(byte[] input, int start, int len) 
     { 
         try { 
   
-            // Static getInstance method is called with hashing MD5 
-            MessageDigest md = MessageDigest.getInstance("MD5"); 
-  
-            // digest() method is called to calculate message digest 
-            //  of an input digest() return array of byte 
-            byte[] messageDigest = md.digest(input.getBytes()); 
-  
-            // Convert byte array into signum representation 
-            BigInteger no = new BigInteger(1, messageDigest); 
-  
-            // Convert message digest into hex value 
-            String hashtext = no.toString(16); 
-            while (hashtext.length() < 32) { 
-                hashtext = "0" + hashtext; 
-            } 
-            return hashtext; 
+            MessageDigest md = MessageDigest.getInstance("SHA1"); 
+            md.update(input, start, len);
+            return md.digest(); 
         }  
-  
-        // For specifying wrong message digest algorithms 
         catch (NoSuchAlgorithmException e) { 
             throw new RuntimeException(e); 
         } 
-    } 
+    }
 }
 
 class TransmissionHelperSender{
@@ -315,11 +341,12 @@ class TransmissionHelperSender{
 	        			byte[] buf = new byte[MyReliableUDPSocket.MAX_PACKET_SIZE];
 	        			DatagramPacket recv = new DatagramPacket(buf, buf.length);
 	        			sock.receive(recv);
+        				if(MyReliableUDPSocket.isCorruptPacket(recv))
+        					continue;
 	        			int seqNo = MyReliableUDPSocket.getIntFromByteArray(buf, 0);
 	        			long pID = MyReliableUDPSocket.getLongFromByteArray(buf ,4);
-	        			System.out.println("Received a packet "+ pID + " Seq No "+ seqNo);
 	        			if(seqNo<0){
-	        				System.out.println("Received acknowledgement "+ pID + " Seq No "+ seqNo);
+	        				// System.out.println("Received acknowledgement "+ pID + " Seq No "+ seqNo);
 	        				if(packetID == pID){
 	        					ackStatus.put(seqNo*-1, true);
 	        				}
@@ -333,6 +360,7 @@ class TransmissionHelperSender{
         				return;
         			}
         		}
+        		System.out.println("\n**File transferred successfully**\nThank you for using MyReliableUDPSocket");
         	}
         };
         threadExit = false;
